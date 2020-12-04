2d0ffbe036dfe072cb845fe64d2b429662f2e668
“Vasya <jedix1488@gmail.com>
2020-12-04 20:09:59 +0200

    +burger
    +burger

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('burgerr').onclick = function() {
+    document.getElementById('burger-intro').classList.toggle('active');
+    document.getElementById('burgerr').classList.toggle('active');
+    document.getElementById('body').classList.toggle('locked');
+
+}
+  
